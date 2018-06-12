5dbc0f1596d905f8ced16deacd0338946c20237a
tam-sah <tam-sah@mail.ru>
2018-06-12 15:41:52 +0400

    because
    security

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 
  return [
             'host' => 'localhost',
-            'name' => 'wm65389_xax',
-            'username' => 'wm65389_rafael',
-            'password' => '123123',
+            'name' => '',
+            'username' => '',
+            'password' => '',
             'charset' => 'utf8'
         ];
- //http://148.251.195.79/
+ 
